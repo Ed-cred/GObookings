6ba1f447109b377fb1a2f18b86cf2e78277b139f
Ed-cred <edwardandrew63@gmail.com>
2023-07-09 17:40:59 +0300

    Fixed typos in routes

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -35,35 +35,34 @@ func (rep *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	rep.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
-	
+
 }
 
 func (rep *Repository) Generals(w http.ResponseWriter, r *http.Request) {
 
-
 	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
-	
+
 }
 func (rep *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	
+
 	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
-	
+
 }
 func (rep *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	
+
 	render.RenderTemplate(w, "search_availability.page.tmpl", &models.TemplateData{})
-	
+
 }
 
 func (rep *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	
+
 	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
-	
+
 }
 func (rep *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	
-	render.RenderTemplate(w, "makre_reservation.page.tmpl", &models.TemplateData{})
-	
+
+	render.RenderTemplate(w, "make_reservation.page.tmpl", &models.TemplateData{})
+
 }
 func (rep *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
