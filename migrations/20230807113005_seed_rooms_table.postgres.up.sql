b0f51d1039b577c644fb4eec42ea13d3e634ac90
Ed-cred <edwardandrew63@gmail.com>
2023-08-07 14:35:36 +0300

    Added migrations for seeding rooms and restrictions

---
+++ migrations/20230807113005_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2023-07-19 00:00:00.000','2023-07-19 00:00:00.000'),
+	 ('Major''s Suite','2023-07-19 00:00:00.000','2023-07-19 00:00:00.000');
---
+++ migrations/20230807113334_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2023-07-19 00:00:00','2023-07-19 00:00:00'),
+	 ('OwnerBlock','2023-07-19 00:00:00','2023-07-19 00:00:00');
